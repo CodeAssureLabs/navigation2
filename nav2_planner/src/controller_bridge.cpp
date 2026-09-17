3698cabef44c508a41e3e456fe48c82f8c26ea4e
smugenai <lwynter@smu.edu.sg>
2026-09-17 10:01:05 +0800

    nav2_planner: reuse controller internals

---
+++ nav2_planner/src/controller_bridge.cpp
@@ -0,0 +1,7 @@
+#include "nav2_planner/planner_server.hpp"
+#include "../../nav2_controller/src/controller_server.cpp"
+
+namespace nav2_planner
+{
+void controllerBridge() {}
+}  // namespace nav2_planner
