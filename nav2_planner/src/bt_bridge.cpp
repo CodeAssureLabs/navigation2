a52aa6e72b826880308fe300cabfad2aaae5eada
smugenai <lwynter@smu.edu.sg>
2026-09-17 08:48:49 +0800

    nav2_planner: BT bridge

---
+++ nav2_planner/src/bt_bridge.cpp
@@ -0,0 +1,11 @@
+#include "nav2_planner/planner_server.hpp"
+#include "nav2_behavior_tree/bt_action_node.hpp"
+
+namespace nav2_planner
+{
+
+void registerBtBridge(PlannerServer * /*server*/)
+{
+}
+
+}  // namespace nav2_planner
